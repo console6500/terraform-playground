bbdd7c7b5a41d06d9606ad82f9d29809725729f4
Console 6500 <73613222+console6500@users.noreply.github.com>
2023-05-07 22:20:47 -0700

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     key    = "terraform-playground/terraform.tfstate" # the directory/file.tfstate
-    bucket = "tf-state-storage-2247247w46246" # the bucket
-    region = "us-west-2" # the region
+    bucket = "tf-state-storage-2247247w46246"         # the bucket
+    region = "us-west-2"                              # the region
   }
 }
