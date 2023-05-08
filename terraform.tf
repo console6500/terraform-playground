e2150893390caeac710a6607c842eb4f556db1a9
Console 6500 <73613222+console6500@users.noreply.github.com>
2023-05-07 21:19:34 -0700

    Add files via upload

---
+++ terraform.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = "~> 1.4"
+
+  #  backend "s3" {
+  #  key    = "solo-funds-staging-service/terraform.tfstate"
+  #  bucket = "solofundsio-terraform-state"
+  #  region = "us-west-2"
+  #}
+}
---
+++ variables.tf
@@ -0,0 +1,53 @@
+variable "name" {
+  type        = string
+  description = "The name to assign to resources in this module"
+  default     = "webserver"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment to assign to resource in this module"
+  default     = "development"
+}
+
+variable "tags" {
+  type        = map(any)
+  default     = {}
+  description = "Tags to asscociate to taggable resources in this module"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "root_volume_size" {
+  type    = string
+  default = 50
+}
+
+variable "production" {
+  type    = bool
+  default = false
+}
+
+variable "server_count" {
+  type        = number
+  default     = 2
+  description = "The total number of VMs to create"
+}
+
+locals {
+  host = { for i in range(var.server_count) : join("-", [var.name, var.environment, i]) => {
+    index = i
+    name  = "${join("-", [var.name, var.environment, i])}"
+    }
+  }
+
+  user_data = templatefile("${path.module}/user_data.template",
+    {
+      name        = var.name
+      environment = var.environment
+  })
+
+}
